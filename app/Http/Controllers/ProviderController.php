8e35ce3030028fbf14aa71048fc7f6f5a96f66e7
Victor H. Olvera <vhocar@gmail.com>
2021-06-11 09:21:47 -0700

    Added ProviderController.

---
+++ app/Http/Controllers/ProviderController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Provider;
+use Illuminate\Http\Request;
+
+class ProviderController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Provider  $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Provider $provider)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Provider  $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Provider $provider)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Provider  $provider
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Provider $provider)
+    {
+        //
+    }
+}
