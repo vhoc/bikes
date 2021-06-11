721956985eebea9a6d2f84743a55d6e859c4539b
Victor H. Olvera <vhocar@gmail.com>
2021-06-11 15:55:12 -0700

    Added [ProductResponse@store] method.

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use App\Http\Requests\StoreProductRequest;
 
 class ProductController extends Controller
 {
@@ -29,6 +30,35 @@ class ProductController extends Controller
     public function store(StoreProductRequest $request)
     {
         $validated = $request->validated();
+
+        $newProduct = new Product;
+
+        $newProduct->product_type_id    = $request->product_type_id;
+        $newProduct->product_brand_id   = $request->product_brand_id;
+        $newProduct->model              = $request->model;
+        $newProduct->year_model         = $request->year_model;
+        $newProduct->serial             = $request->serial;
+        $newProduct->wheel_size         = $request->wheel_size;
+        $newProduct->color_id           = $request->color_id;
+        $newProduct->description        = $request->description;
+        $newProduct->images             = $request->images;
+        $newProduct->acquired           = $request->acquired;
+        $newProduct->last_maintenance   = $request->last_maintenance;
+        $newProduct->due_maintenance   = $request->due_maintenance;
+
+        if( $newProduct->save() )
+        {
+            return response( [
+                'message' => 'Product created successfully',
+                'code' => 201
+            ], 201 );
+        }
+        return response( [
+            'message' => 'Error creating product.',
+            'code' => 400
+        ] );
+
+
     }
 
     /**
